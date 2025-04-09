3ddaf0c8227de1a06ab6ea1aac8be2df96400ffa
ARCHER\sray <sray@archer.com>
2025-04-08 17:15:50 -0700

    Title : Timer-0 interrupt
    - Timer-0 interrupt, service request working.
    - Timer-0 is wrapping around for now, need to reset after
      compare is hit so we can change the interrupt rate.
    - The timer_0_isr_count sucessfully incrementing.

---
+++ hello_world_1/TriCore Debug (TASKING)/sources.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C++M_SRCS := 
+C++_SRCS := 
+CCM_SRCS := 
+CC_SRCS := 
+CPP_SRCS := 
+CXXM_SRCS := 
+CXX_SRCS := 
+C_SRCS := 
+C_UPPER_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+SRC_SRCS := 
+C++M_DEPS := 
+C++_DEPS := 
+CCM_DEPS := 
+CC_DEPS := 
+COMPILED_SRCS := 
+CPP_DEPS := 
+CXXM_DEPS := 
+CXX_DEPS := 
+C_DEPS := 
+C_UPPER_DEPS := 
+EXECUTABLES := 
+OBJS := 
+SECONDARY_SIZE := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Configurations/Debug \
+Configurations \
+. \
+Libraries/Infra/Platform/Tricore/Compilers \
+Libraries/Infra/Ssw/TC38A/Tricore \
+Libraries/iLLD/TC38A/Tricore/Cpu/Irq \
+Libraries/iLLD/TC38A/Tricore/Cpu/Std \
+Libraries/iLLD/TC38A/Tricore/Cpu/Trap \
+Libraries/iLLD/TC38A/Tricore/Gtm/Std \
+Libraries/iLLD/TC38A/Tricore/Mtu/Std \
+Libraries/iLLD/TC38A/Tricore/Pms/Std \
+Libraries/iLLD/TC38A/Tricore/Port/Std \
+Libraries/iLLD/TC38A/Tricore/Scu/Std \
+Libraries/iLLD/TC38A/Tricore/Src/Std \
+Libraries/iLLD/TC38A/Tricore/_Impl \
+Libraries/iLLD/TC38A/Tricore/_PinMap \
+
